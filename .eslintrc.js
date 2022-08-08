aa954016066c85384b4114b949ebeea613fe7723
jhonperez95 <jhonperez.j95@gmail.com>
2022-08-08 16:47:38 -0500

    conectado mongo

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,12 +2,12 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   parserOptions: {
     project: 'tsconfig.json',
-    tsconfigRootDir : __dirname, 
+    tsconfigRootDir: __dirname,
     sourceType: 'module',
   },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
-    'plugin:@typescript-eslint/recommended',
+    // 'plugin:@typescript-eslint/recommended',
     // 'plugin:prettier/recommended',
   ],
   root: true,
@@ -22,4 +22,4 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
   },
-};
+}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
-import { Module } from '@nestjs/common';
+import { join } from 'path'
+import { Module } from '@nestjs/common'
+import { MongooseModule } from '@nestjs/mongoose'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { PokemonModule } from './pokemon/pokemon.module'
 
 @Module({
-  imports: [],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    MongooseModule.forRoot('mongodb://localhost:27017/nest-pokemon'),
+    PokemonModule,
+  ],
   controllers: [],
   providers: [],
 })
